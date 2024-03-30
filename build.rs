a4a5014eaf30696d5fa9cd5b63344d558fdd0734
alexandreyc <alexandre.crayssac@gmail.com>
2024-03-30 18:58:30 +0100

    add readme, license and cargo metadata

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=native=.");
-    println!("cargo:rustc-link-lib=static=adbc_driver_common");
-    println!("cargo:rustc-link-lib=static=adbc_driver_sqlite");
-    println!("cargo:rustc-link-lib=static=nanoarrow");
-    println!("cargo:rustc-link-lib=static=sqlite3");
-}
