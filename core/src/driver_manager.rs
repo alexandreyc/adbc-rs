2d8a08ee5daa51b9db61c3002af4543f4dd346b9
alexandreyc <alexandre.crayssac@gmail.com>
2024-05-07 10:03:11 +0200

    rename `DriverManagerInner` to `ManagedDriverInner`

--- core/src/driver_manager.rs
+++ core/src/driver_manager.rs
@@ -132,7 +132,7 @@ impl From<libloading::Error> for Error {
     }
 }
 
-struct DriverManagerInner {
+struct ManagedDriverInner {
     driver: Mutex<ffi::FFI_AdbcDriver>,
     version: AdbcVersion, // Driver version
     _library: Option<libloading::Library>,
@@ -141,14 +141,14 @@ struct DriverManagerInner {
 /// Implementation of [Driver].
 #[derive(Clone)]
 pub struct ManagedDriver {
-    inner: Arc<DriverManagerInner>,
+    inner: Arc<ManagedDriverInner>,
 }
 
 impl ManagedDriver {
     /// Load a driver from an initialization function.
     pub fn load_static(init: &ffi::FFI_AdbcDriverInitFunc, version: AdbcVersion) -> Result<Self> {
         let driver = Self::load_impl(init, version)?;
-        let inner = Arc::new(DriverManagerInner {
+        let inner = Arc::new(ManagedDriverInner {
             driver: Mutex::new(driver),
             version,
             _library: None,
@@ -176,7 +176,7 @@ impl ManagedDriver {
         let init: libloading::Symbol<ffi::FFI_AdbcDriverInitFunc> =
             unsafe { library.get(entrypoint)? };
         let driver = Self::load_impl(&init, version)?;
-        let inner = Arc::new(DriverManagerInner {
+        let inner = Arc::new(ManagedDriverInner {
             driver: Mutex::new(driver),
             version,
             _library: Some(library),
@@ -384,7 +384,7 @@ where
 
 struct ManagedDatabaseInner {
     database: Mutex<ffi::FFI_AdbcDatabase>,
-    driver: Arc<DriverManagerInner>,
+    driver: Arc<ManagedDriverInner>,
 }
 
 impl Drop for ManagedDatabaseInner {
